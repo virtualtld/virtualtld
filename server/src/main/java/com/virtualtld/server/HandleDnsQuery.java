1f9d4b7823b2a45a5e8ae40db63488f3bd97a12f
Virtual Tld <virtualtld@protonmail.com>
2020-02-04 07:44:26 -0800

    query txt less than 255

--- server/src/main/java/com/virtualtld/server/HandleDnsQuery.java
+++ server/src/main/java/com/virtualtld/server/HandleDnsQuery.java
@@ -3,19 +3,29 @@ package com.virtualtld.server;
 import org.xbill.DNS.DClass;
 import org.xbill.DNS.Message;
 import org.xbill.DNS.Name;
+import org.xbill.DNS.Rcode;
 import org.xbill.DNS.Record;
 import org.xbill.DNS.Section;
+import org.xbill.DNS.TXTRecord;
 import org.xbill.DNS.Type;
 
+import java.util.Map;
 import java.util.function.Function;
 
 public class HandleDnsQuery implements Function<Message, Message> {
 
     private final Message nsResp;
-    private Name myName;
+    private final Map<String, byte[]> chunks;
+    private final Name myName;
 
-    public HandleDnsQuery(Message nsResp) {
+    public HandleDnsQuery(Message nsResp, Map<String, byte[]> chunks) {
         this.nsResp = nsResp;
+        this.chunks = chunks;
+        if (nsResp != null) {
+            myName = nsResp.getSectionArray(Section.ANSWER)[0].getName();
+        } else {
+            myName = null;
+        }
     }
 
     @Override
@@ -28,15 +38,33 @@ public class HandleDnsQuery implements Function<Message, Message> {
     }
 
     private Message _apply(Message input) throws Exception {
-        myName = this.nsResp.getSectionArray(Section.ANSWER)[0].getName();
         Record question = input.getQuestion();
         if (question.getName().equals(myName) && question.getDClass() == DClass.IN && question.getType() == Type.NS) {
             Message output = (Message) nsResp.clone();
             output.getHeader().setID(input.getHeader().getID());
             return output;
         }
-        Message output = new Message();
-        output.getHeader().setID(input.getHeader().getID());
+        if (question.getDClass() == DClass.IN && question.getType() == Type.TXT) {
+            return queryTxt(input);
+        }
+        Message err = new Message(input.getHeader().getID());
+        err.getHeader().setRcode(Rcode.NXDOMAIN);
+        return err;
+    }
+
+    private Message queryTxt(Message input) {
+        Message output = new Message(input.getHeader().getID());
+        String digest = input.getQuestion().getName().getLabelString(0);
+        byte[] chunk = chunks.get(digest);
+        if (chunk == null) {
+            output.getHeader().setRcode(Rcode.NXDOMAIN);
+            return output;
+        }
+        byte[] record1Bytes = new byte[chunk.length + 1];
+        record1Bytes[0] = (byte) chunk.length;
+        System.arraycopy(chunk, 0, record1Bytes, 1, chunk.length);
+        TXTRecord record1 = (TXTRecord) Record.newRecord(input.getQuestion().getName(), Type.TXT, DClass.IN, 172800, record1Bytes);
+        output.addRecord(record1, Section.ANSWER);
         return output;
     }
 }
--- server/src/test/java/com/virtualtld/server/HandleDnsQueryTest.java
+++ server/src/test/java/com/virtualtld/server/HandleDnsQueryTest.java
@@ -10,6 +10,7 @@ import org.xbill.DNS.Section;
 import org.xbill.DNS.Type;
 
 import java.net.IDN;
+import java.util.HashMap;
 
 public class HandleDnsQueryTest {
 
@@ -21,8 +22,18 @@ public class HandleDnsQueryTest {
         Message nsResp = new Message();
         nsResp.addRecord(Record.newRecord(name, Type.NS, DClass.IN, 1024,
                 Name.fromString("ver.1.1.virtualtld.com.").toWire()), Section.ANSWER);
-        Message output = new HandleDnsQuery(nsResp).apply(input);
+        Message output = new HandleDnsQuery(nsResp, new HashMap<>()).apply(input);
         nsResp.getHeader().setID(1029);
         Assert.assertEquals(nsResp.toString(), output.toString());
+    }
+
+    @Test
+    public void query_txt_less_than_255() throws Exception {
+        Name name = Name.fromString(IDN.toASCII("abcd.最新版本.xyz."));
+        Message input = Message.newQuery(Record.newRecord(name, Type.TXT, DClass.IN));
+        Message output = new HandleDnsQuery(null, new HashMap<String, byte[]>() {{
+            put("abcd", new byte[]{1, 2, 3, 4});
+        }}).apply(input);
+        Assert.assertNotEquals(-1, output.toString().indexOf("\"\\001\\002\\003\\004\""));
     }
 }
